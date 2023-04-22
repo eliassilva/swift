3745c7f761420dcb28c2075b3613a853ad385b13
Elias Silva <eliassilva@eliassilva.me>
2023-04-22 20:59:58 -0300

    fix app3: finish

--- app3/app3/ContentView.swift
+++ app3/app3/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var beforeTaxPrice = ""
+    @State var salesTaxRate = ""
+    @State var afterTaxPrice = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Sales Tax Calculator")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+            HStack {
+                Text("Before Tax Price")
+                TextField("$0", text: $beforeTaxPrice).keyboardType(.decimalPad)
+            }
+            .padding(.top, 40.0)
+            HStack {
+                Text("Sales Tax Rate")
+                TextField("3.7%", text: $salesTaxRate).keyboardType(.decimalPad)
+            }
+            .padding(.top, 20.0)
+            HStack {
+                Text("After Tax Price")
+                TextField("$0", text: $afterTaxPrice).disabled(true)
+            }
+            .padding(.top, 20.0)
+            Button("Calculate") {
+                if let beforeTax = Float(beforeTaxPrice), let tax = Float(salesTaxRate) {
+                    let salesTax = beforeTax * tax / 100
+                    afterTaxPrice = String(beforeTax + salesTax)
+                }
+            }.padding(.top, 40)
+            Spacer()
+        }.padding()
     }
 }
 
