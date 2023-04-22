b504fe1b7f19f73afb524bc2a04529effd9b027c
Elias Silva <eliassilva@eliassilva.me>
2023-04-22 20:32:44 -0300

    feat app2: sales tax calculator

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # swift
+
+swift training
+
+## APP1
+
+APP1 swift training macos app with storyboard
+
+### Course:
+
+- macos 10 development fundamentals
+
+## APP2
+
+APP2 swift training ios app with storyboard
+### Course:
+
+- ios app development the big picture
---
+++ app2/app2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app2
+//
+//  Created by Elias Silva on 22/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
